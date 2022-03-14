bf70a84360feb70e41d6853e7870839b155a3dff
adisve <adis.veletanlic@gmail.com>
2022-03-14 15:43:38 +0100

    Add SQL files in migrations folder (up/down)

--- migrations/2022-03-14-131431_create_users/up.sql
+++ migrations/2022-03-14-131431_create_users/up.sql
@@ -1,1 +1,36 @@
 -- Your SQL goes here
+CREATE TABLE IF NOT EXISTS `pillow_db`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(40) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(30) NULL,
+  PRIMARY KEY (`user_id`))
+
+CREATE TABLE IF NOT EXISTS `pillow_db`.`phone` (
+  `imei` CHAR(15) NOT NULL,
+  `uuid` CHAR(38) NOT NULL,
+  `mac` CHAR(12) NOT NULL,
+  `brand` VARCHAR(45) NULL,
+  `model` VARCHAR(30) NULL,
+  `manufacturer` VARCHAR(30) NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`imei`),
+  INDEX `fk_Phone_User_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Phone_User`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `pillow_db`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+CREATE TABLE IF NOT EXISTS `pillow_db`.`schedule` (
+  `schedule_id` INT NOT NULL AUTO_INCREMENT,
+  `alarm_date` VARCHAR(40) NULL,
+  `user_id` INT NOT NULL,
+  `schedule_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`schedule_id`),
+  INDEX `fk_Schedule_User1_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Schedule_User1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `pillow_db`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use std::env;
 
 mod schema;
 mod models;
+pub mod query_routes;
 
 fn main() {
     dotenv().ok();
@@ -19,13 +20,24 @@ fn main() {
     let url = env::var("DATABASE_URL").expect("set URL");
     let conn = &MysqlConnection::establish(&url).unwrap();
 
+    let result = models::User::show(1, &conn);
+
+    for user in result{
+        println!("{}", user.name);
+        println!("----------\n");
+        println!("{}", user.email);
+        println!("----------\n");
+        println!("{}", user.password);
+    }
+
+
     let user = models::NewUser {
         email : String::from("adis.veletanlic@gmail.com"),
         password : String::from("P@sSw0r_D"),
         name : String::from("Adis Veletanlic"),
     };
 
-    if models::User::insert(user, &conn){
+    if query_routes::User::insert(user, &conn){
         println!("Successful insertion");
     }else{
         println!("Failed to insert user");
